bb466f8ef4c929a922ba9ddcc74efa6e034ad0db
Glory Quan <gloryquan2@gmail.com>
2022-02-20 23:41:31 -0800

    Set up generate meeting time button on team page

--- app/src/main/java/ca/group6/meetmatcher/TeamPage.kt
+++ app/src/main/java/ca/group6/meetmatcher/TeamPage.kt
@@ -2,10 +2,12 @@ package ca.group6.meetmatcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import ca.group6.meetmatcher.fragments.AvailabilityFragment
 import ca.group6.meetmatcher.fragments.HomeFragment
 import ca.group6.meetmatcher.fragments.ProfileFragment
+import kotlinx.android.synthetic.main.activity_team_page.*
 import kotlinx.android.synthetic.main.activity_toolbar.*
 import kotlinx.android.synthetic.main.app_bar.*
 
@@ -15,12 +17,32 @@ class TeamPage : AppCompatActivity() {
     private val profileFragment = ProfileFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val groupName = "A Team"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team_page)
-        setUpToolbar()
+        setUpToolbar(groupName)
+        setUpGenerateMeetingButton(groupName)
     }
 
-    private fun setUpToolbar() {
+    private fun setUpGenerateMeetingButton(groupName : String) {
+        FABmeeting.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Use current availability?")
+
+            builder.setPositiveButton("YES") { dialog, _ ->
+
+            }
+            builder.setNegativeButton("EDIT") { dialog, _->
+                replaceFragment(availabilityFragment)
+            }
+
+            val dialog : AlertDialog = builder.create()
+            dialog.show()
+        }
+    }
+
+    private fun setUpToolbar(groupName : String) {
+        toolbar_title.text = groupName
         bottom_navigation.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.ic_home -> {
@@ -39,6 +61,7 @@ class TeamPage : AppCompatActivity() {
             true
         }
     }
+
     private fun replaceFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, fragment)
