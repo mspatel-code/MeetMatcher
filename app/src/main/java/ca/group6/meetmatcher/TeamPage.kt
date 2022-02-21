29b7c0ce21ee13f70fb1ac50608d2c78dff90f66
cindy <cindy.yeyuxin@gmail.com>
2022-02-20 19:05:28 -0800

    Create team page and member list fragment

---
+++ app/src/main/java/ca/group6/meetmatcher/TeamPage.kt
@@ -0,0 +1,12 @@
+package ca.group6.meetmatcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeamPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_page)
+
+    }
+}
