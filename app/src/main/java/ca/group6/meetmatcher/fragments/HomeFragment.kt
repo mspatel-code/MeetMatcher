a18e31d006edb8cf014443240932354df99e409c
cindy <cindy.yeyuxin@gmail.com>
2022-03-15 16:59:01 -0700

    Added team members to team list page

--- app/src/main/java/ca/group6/meetmatcher/fragments/HomeFragment.kt
+++ app/src/main/java/ca/group6/meetmatcher/fragments/HomeFragment.kt
@@ -1,11 +1,17 @@
 package ca.group6.meetmatcher.fragments
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ca.group6.meetmatcher.R
 import ca.group6.meetmatcher.TeamPage
 import ca.group6.meetmatcher.databinding.FragmentTeamListPageBinding
@@ -15,6 +21,8 @@ import ca.group6.meetmatcher.databinding.FragmentTeamListPageBinding
 class HomeFragment : Fragment() {
     private var _binding: FragmentTeamListPageBinding? = null
     private val binding get() = _binding!!
+    var memberList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,6 +34,13 @@ class HomeFragment : Fragment() {
     ): View? {
         _binding = FragmentTeamListPageBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        //Arraylist
+        memberList = arrayListOf("Member A", "Member B,", "Member C", "Member D")
+        println(memberList)
+
+        binding.myRv.layoutManager = LinearLayoutManager (activity as Context)
+        binding.myRv.adapter = MyAdapter(memberList)
         return view
     }
 
@@ -42,5 +57,25 @@ class HomeFragment : Fragment() {
             transaction?.disallowAddToBackStack()
             transaction?.commit()
         }
+    }
+
+    class MyViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.fragment_team_list, parent, false))
+
+    internal inner class MyAdapter(private val array:ArrayList<String>) :
+        RecyclerView.Adapter<MyViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            return MyViewHolder(inflater, parent)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            (holder.itemView as LinearLayout).findViewById<TextView>(R.id.teamName).text = array[position]
+
+        }
+
+        override fun getItemCount() = array.size
+
     }
 }
