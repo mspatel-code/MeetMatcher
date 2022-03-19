7791632b8e3a0ad1928ca46c4c3c00219f4b0c5d
Skyler <xla276@sfu.ca>
2022-03-19 03:17:58 -0700

    Enable back button

--- app/src/main/java/ca/group6/meetmatcher/ToolbarActivity.kt
+++ app/src/main/java/ca/group6/meetmatcher/ToolbarActivity.kt
@@ -2,9 +2,19 @@ package ca.group6.meetmatcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import ca.group6.meetmatcher.databinding.ActivityToolbarBinding
 import ca.group6.meetmatcher.fragments.*
+import kotlinx.coroutines.internal.artificialFrame
 
 class ToolbarActivity : AppCompatActivity(), OnMakeMeetingButtonTapListener, OnEditAvailabilityButtonTapListener, OnFinishSelectTimeListener {
     private lateinit var binding: ActivityToolbarBinding
@@ -17,45 +27,55 @@ class ToolbarActivity : AppCompatActivity(), OnMakeMeetingButtonTapListener, OnE
         super.onCreate(savedInstanceState)
         binding = ActivityToolbarBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(homeFragment, "homeTag")
 
-       binding.bottomNavigation.setOnNavigationItemSelectedListener {
+        binding.toolbarLayout.backButton.setOnClickListener {
+            val last = supportFragmentManager.backStackEntryCount-1
+            val tag = supportFragmentManager.getBackStackEntryAt(last).name
+            val fragment = supportFragmentManager.findFragmentByTag(tag)
+            if (fragment != null && tag != null) {
+                replaceFragment(fragment, tag)
+            }
+        }
+
+        replaceFragment(homeFragment, getString(R.string.bottom_ic_home))
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.ic_home -> {
-                    replaceFragment(homeFragment, "homeTag")
-                    binding.toolbarTitle.text = getString(R.string.bottom_ic_home)
+                    replaceFragment(homeFragment, getString(R.string.bottom_ic_home))
                 }
                 R.id.ic_availability -> {
-                    replaceFragment(availabilityFragment, "availabilityTag")
-                    binding.toolbarTitle.text = getString(R.string.bottom_ic_availability)
+                    replaceFragment(availabilityFragment, getString(R.string.bottom_ic_availability))
                 }
                 R.id.ic_profile -> {
-                    replaceFragment(profileFragment, "profileTag")
-                    binding.toolbarTitle.text = getString(R.string.bottom_ic_profile)
+                    replaceFragment(profileFragment, getString(R.string.bottom_ic_profile))
                 }
             }
             true
         }
     }
 
-    private fun replaceFragment(fragment: Fragment, tag: String) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment, tag)
-        transaction.commit()
+    private fun replaceFragment(fragment: Fragment, title: String) {
+        val tran = supportFragmentManager
+        tran.beginTransaction()
+            .replace(R.id.fragment_container, fragment, title)
+            .addToBackStack(title)
+            .commit()
+        tran.executePendingTransactions()
+        binding.toolbarLayout.toolbarTitle.text = title
     }
 
     override fun OnMakeMeetingButtonTapped(times: Array<String>) {
-        replaceFragment(SelectTime.newInstance(times), "selectTimeTag")
+        replaceFragment(SelectTime.newInstance(times), "Select Time")
     }
 
     override fun OnEditAvailabilityButtonTapped() {
-        replaceFragment(availabilityFragment, "availabilityTag")
+        replaceFragment(availabilityFragment, "Availability")
     }
 
     override fun OnFinishSelectTimeTapped(times: Array<String>?) {
         // get first time for now
 //        var time: String = times[0]
 //        var meeting: Meeting = Meeting(time,"")
-        replaceFragment(TeamPage.newInstance("Feb. 15 19:00 - 20:00"), "teamPageTag")
+        replaceFragment(TeamPage.newInstance("Feb. 15 19:00 - 20:00"), "Team Page")
     }
 }
