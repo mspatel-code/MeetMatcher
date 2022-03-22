8c95235e65b9baed5391fa02b8aa21fa0367f0c3
Skyler <xla276@sfu.ca>
2022-03-22 00:38:40 -0700

    change password and nickname

--- app/src/main/java/ca/group6/meetmatcher/fragments/ChangePasswordFragment.kt
+++ app/src/main/java/ca/group6/meetmatcher/fragments/ChangePasswordFragment.kt
@@ -1,11 +1,15 @@
 package ca.group6.meetmatcher.fragments
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import ca.group6.meetmatcher.databinding.FragmentChangePasswordBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ChangePasswordFragment : Fragment() {
     private var _binding: FragmentChangePasswordBinding? = null
@@ -18,16 +22,47 @@ class ChangePasswordFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentChangePasswordBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.cancelChangePassword.setOnClickListener {
-            val transaction = activity?.supportFragmentManager?.beginTransaction()
-            transaction?.remove(this)
-            transaction?.commit()
+        val user = FirebaseAuth.getInstance().currentUser
+
+        binding.submitChangePassword.setOnClickListener {
+            val newPassword = binding.inputNewPassword.text.toString()
+            val repeatNewPassword = binding.inputRepeatPassword.text.toString()
+
+            var errorMsg = ""
+            if (newPassword != repeatNewPassword) {
+                errorMsg = "Please reconfirm your password."
+            } else if (newPassword.isEmpty()) {
+                errorMsg = "Please enter new password."
+            }
+
+            if (errorMsg.isNotEmpty()) {
+                Toast.makeText(activity, errorMsg, Toast.LENGTH_LONG).show()
+            } else {
+                user!!.updatePassword(newPassword).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(activity, "Updated!", Toast.LENGTH_LONG).show()
+                        transToProfile()
+                    } else {
+                        Log.w(ContentValues.TAG, "ChangePassword:taskFail", task.exception)
+                    }
+                }
+            }
         }
+
+        binding.cancelChangePassword.setOnClickListener {
+            transToProfile()
+        }
+    }
+
+    private fun transToProfile() {
+        val transaction = activity?.supportFragmentManager?.beginTransaction()
+        transaction?.remove(this)
+        transaction?.commit()
     }
 }
