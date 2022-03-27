b47bf29c1778078db88ed412882bef1e3a11a3c4
mspatel <moinuddin_salimbhai_patel@sfu.ca>
2022-03-27 16:11:38 -0700

    Common Availability Algorithm - 2 Users

--- app/src/main/java/ca/group6/meetmatcher/adapterClasses/CommonAvailability.kt
+++ app/src/main/java/ca/group6/meetmatcher/adapterClasses/CommonAvailability.kt
@@ -1,40 +1,32 @@
-package ca.group6.meetmatcher.adapterClasses;
+package ca.group6.meetmatcher.adapterClasses
 
-import java.util.*;
+import java.util.ArrayList
 
-class Interval {
-    int start;
-    int end;
-
-    public Interval(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-};
-
-class IntervalsIntersection {
-
-    public static Interval[] merge(Interval[] arr1, Interval[] arr2) {
-        List<Interval> result = new ArrayList<Interval>();
-        int i = 0, j = 0;
-
-        while (i < arr1.length && j < arr2.length) {
+internal class Interval(var start: Int, var end: Int)
+internal object IntervalsIntersection {
+    fun merge(arr1: Array<Interval>, arr2: Array<Interval>): Array<Interval> {
+        val result: MutableList<Interval> = ArrayList()
+        var i = 0
+        var j = 0
+        while (i < arr1.size && j < arr2.size) {
             // check if the interval arr[i] intersects with arr2[j]
             // check if one of the interval's start time lies within the other interval
-
-            if ((arr1[i].start >= arr2[j].start && arr1[i].start <= arr2[j].end)
-                    || (arr2[j].start >= arr1[i].start && arr2[j].start <= arr1[i].end)) {
+            if (arr1[i].start >= arr2[j].start && arr1[i].start <= arr2[j].end
+                || arr2[j].start >= arr1[i].start && arr2[j].start <= arr1[i].end
+            ) {
                 // store the intersection part
-                result.add(new Interval(Math.max(arr1[i].start, arr2[j].start), Math.min(arr1[i].end, arr2[j].end)));
+                result.add(
+                    Interval(
+                        Math.max(arr1[i].start, arr2[j].start), Math.min(
+                            arr1[i].end, arr2[j].end
+                        )
+                    )
+                )
             }
 
             // move next from the interval which is finishing first
-            if (arr1[i].end < arr2[j].end)
-                i++;
-            else
-                j++;
+            if (arr1[i].end < arr2[j].end) i++ else j++
         }
-
-        return result.toArray(new Interval[result.size()]);
+        return result.toTypedArray()
     }
 }
