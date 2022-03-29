7c0b408b61fc6a5ed68c1681ca50d0e594b2972a
cindy <cindy.yeyuxin@gmail.com>
2022-03-29 15:16:00 -0700

    Prevents crashing if user have no teams

--- app/src/main/java/ca/group6/meetmatcher/fragments/HomeFragment.kt
+++ app/src/main/java/ca/group6/meetmatcher/fragments/HomeFragment.kt
@@ -2,12 +2,16 @@ package ca.group6.meetmatcher.fragments
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Color.rgb
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,6 +37,7 @@ class HomeFragment : Fragment() {
     companion object {
         var auth: FirebaseAuth = FirebaseAuth.getInstance()
         var memberList = ArrayList<String>()
+        var statusList = ArrayList<String>()
         public var myTeams : MutableList<String> = ArrayList()
         val database = Firebase.database
     }
@@ -56,9 +61,9 @@ class HomeFragment : Fragment() {
         val view = binding.root
 
         //Arraylist
-        //retrieveTeam()
+        retrieveTeam()
         Log.i("Home", "Retrieved team")
-        memberList = arrayListOf("Member A", "Member B", "Member C", "Member D")
+        //memberList = arrayListOf("Member A", "Member B", "Member C", "Member D")
 
 
         binding.myRv.layoutManager = LinearLayoutManager (activity as Context)
@@ -83,52 +88,75 @@ class HomeFragment : Fragment() {
     }
 
     fun retrieveTeam() {
-        //myTeams.add("Team B")
+        Log.i("retrieveTeam", "Starting")
+        val path = database.reference.child("Teams").child(myUid)
+            if (path != null) {
+                Log.i("retrieveTeam", "path is not null")
+                path.addListenerForSingleValueEvent(object : ValueEventListener {
 
-            database.reference.child("Teams").child(myUid)
-                .addListenerForSingleValueEvent(object : ValueEventListener {
-
-                    override fun onDataChange(p0: DataSnapshot) {
-                        memberList.clear()
-                       // val myMap = p0.value as HashMap<String, String>
+                        override fun onDataChange(p0: DataSnapshot) {
+                            memberList.clear()
+                            statusList.clear()
+                            // val myMap = p0.value as HashMap<String, String>
 
 //                        val myMap = p0.value as HashMap<String, String>
 //                        val myKey = myMap.keys.first()
 
-                        //Get the values from p0 into a hashmap
-                        val HashMapOfAllThings = p0.value as HashMap<String, Any>
-                        //Log.i("retrieveTeam", HashMapOfAllThings.values.toString())
-                        //For each key in the hashmap
-                        for (anyKey in HashMapOfAllThings.keys) {
+                            //Get the values from p0 into a hashmap
+                            if (p0.value != null) {
+                                val HashMapOfAllThings = p0.value as HashMap<String, Any>
 
-                        //Retrieve team name
-                        Log.i("retrieveTeam", anyKey)
-                            binding.teamTitle.text = anyKey
+                                //For each key in the hashmap
+                                for (anyKey in HashMapOfAllThings.keys) {
 
-                        val one = HashMapOfAllThings.get(anyKey) as HashMap<String, Any>
-                            //Log.i("printHashMap", one.toString())
+                                    //Retrieve team name
+                                    Log.i("retrieveTeam", anyKey)
+                                    if (anyKey != null) {
+                                        binding.teamTitle.text = anyKey
+                                    } else {
+                                        binding.teamTitle.text = "My Teams"
+                                    }
 
-                            for (keys in one.keys) {
-                                val two = one.get(keys) as HashMap<String, Any>
-                                Log.i("Two", two["username"].toString())
-                                memberList.add(two["username"].toString())
+
+                                    val one = HashMapOfAllThings.get(anyKey) as HashMap<String, Any>
+                                    //Log.i("printHashMap", one.toString())
+
+                                    for (keys in one.keys) {
+                                        val two = one.get(keys) as HashMap<String, Any>
+                                        //Log.i("Two", two["username"].toString())
+                                        if (two["username"] != null) {
+                                            memberList.add(two["username"].toString())
+                                            statusList.add(two["status"].toString())
+                                            binding.myRv.adapter!!.notifyDataSetChanged()
+                                        } else {
+                                            memberList.add("You do not have any team members.")
+                                            binding.myRv.adapter!!.notifyDataSetChanged()
+                                        }
+
+                                    }
+                                    //Log.i("printHashMap", one["username"].toString())
+                                }
+                            } else {
+                                Log.i("null", "p0 is null")
+                                binding.teamTitle.text = "My Teams"
+                                memberList.add("You do not have any team members.")
                                 binding.myRv.adapter!!.notifyDataSetChanged()
                             }
-                        //Log.i("printHashMap", one["username"].toString())
-                    }
-//                        Log.i("retrieveTeamUsername", p0.child("username").getValue(String::class.java).toString())
-//                        for (snapshot in p0.children) {
-//
-//                            Log.i("inLoop", snapshot.child("username").getValue(String::class.java).toString())
-//                        }
 
-                    }
+                        }
 
-                    override fun onCancelled(error: DatabaseError) {
+                        override fun onCancelled(error: DatabaseError) {
 
-                    }
+                        }
 
-                })
+                    })
+            } else {
+                Log.i("null", "path is null")
+                binding.teamTitle.text = "My Teams"
+                memberList = arrayListOf("You do not have any team members.")
+
+            }
+
 
 
     }
@@ -148,6 +176,28 @@ class HomeFragment : Fragment() {
 
         override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
             (holder.itemView as LinearLayout).findViewById<TextView>(R.id.teamName).text = array[position]
+            val online = holder.itemView.findViewById<ImageView>(R.id.status_online)
+            val offline = holder.itemView.findViewById<ImageView>(R.id.status_offline)
+            val usernameText = holder.itemView.findViewById<TextView>(R.id.teamName)
+
+            if (memberList.contains("You do not have any team members.")) {
+                online.visibility = View.GONE
+                offline.visibility = View.GONE
+                usernameText.setTextColor(rgb(150, 150, 150))
+
+            } else {
+                var currentStatus: String = statusList.get(position)
+                if (currentStatus == "offline") {
+                    online.visibility = View.GONE
+                    offline.visibility = View.VISIBLE
+                }
+                if (currentStatus == "online") {
+                    online.visibility = View.VISIBLE
+                    offline.visibility = View.GONE
+                }
+                usernameText.setTextColor(Color.BLACK)
+
+            }
 
         }
 
